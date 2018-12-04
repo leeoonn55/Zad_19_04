ef77b7b0faef15f053344b09e2bc7b488f3770f5
Leszek Macura <leszek@macura.co>
2018-12-04 13:05:23 +0100

    Zadanie 19_0 redux i Reducer

---
+++ comments.js
@@ -0,0 +1,39 @@
+import { ADD_COMMENT } from './actions'
+import { EDIT_COMMENT } from './actions'
+import { REMOVE_COMMENT } from './actions'
+import { THUMB_UP_COMMENT } from './actions'
+import { THUMB_DOWN_COMMENT } from './actions'
+
+function comments(state = [], action) {
+	switch (action.type) {
+		case ADD_COMMENT:
+			return [{
+				id: action.id,
+				text: action.text,
+				votes: 0
+			}
+				, ...state]
+		case REMOVE_COMMENT:
+			return [{
+				comments: state.comments.filter(comment => comment.id !== action.id)
+			}]
+		case EDIT_COMMENT:
+			return [{
+				id: action.id,
+				text: action.text
+			}]
+		case THUMB_UP_COMMENT:
+			return [{
+				id: action.id,
+				votes: ++
+			}]
+		case THUMB_DOWN_COMMENT:
+			return [{
+				id: action.id,
+				votes: --
+			}]
+		default:
+			return state;
+	}
+
+}
---
+++ reducer.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
+
+
+const initialState = {
+    comments: [],
+    users: []
+};
+
+
+
+const app = combineReducers({
+    comments,
+    users
+});
